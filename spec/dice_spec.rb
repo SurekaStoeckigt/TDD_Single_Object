0921ee71ee2717869946e52f804de10ef49b9ca5
Student <student@Admins-MacBook-Pro-3.local>
2019-02-08 12:46:27 +0000

    end of this core exercise

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,12 +3,12 @@ require 'dice'
 
 describe Dice do
 attr_reader :number
-
+## STORY 2
 it 'responds to roll' do
   dice = Dice.new
   expect(dice).to respond_to(:roll)
 end
-
+## STORY 3
 #it 'rolls the dice and returns number between 1 and 6' do
 #  dice = Dice.new
 #  expect(dice.roll).to be_between(1,6)
@@ -18,14 +18,13 @@ end
 #  dice = Dice.new
 #  expect(dice.roll).to be_between(1,6)
 #end
-
+## STORY 4
 it 'rolls multiple die at the same time' do
   dice = Dice.new(2)
   expect(dice.number).to eq(2)
 end
-
+## STORY 5
 it 'expects each dice to return number between 1 and 6' do
-
   dice = Dice.new(3)
   result = dice.roll
   result.each do |integer|
